3506fe8150f8e0947911d3818382ee26af0a2fb5
Digital-Education-Studio <giles.barber@qmul.ac.uk>
2025-02-27 22:36:34 +0000

    adding gitmodules

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/med-ed-chat-app/',
+  build: {
+    chunkSizeWarningLimit: 1600
+  },
   plugins: [
-    vue(),
+    vue()
   ]
 })
